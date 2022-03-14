f850abaaa12ac8f2aa308ab41e8afd4b4774929b
Md. Emran Hossen <92653363+emranhossen26@users.noreply.github.com>
2022-03-14 11:18:49 +0600

    Add files via upload

---
+++ M_PI.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float r,area;
+    printf("Enrer Any Number:");
+    scanf("%f",&r);
+
+    area=M_PI*r*r;
+    printf("The area iS:%f",area);
+    return 0;
+
+}
