231209a65ea16939cbab8156ef2df07be1ba949f
Md. Emran Hossen <92653363+emranhossen26@users.noreply.github.com>
2022-04-17 03:01:35 +0600

    Add files via upload

--- Array-12(Simple Matrix).c
+++ Array-12(Simple Matrix).c
@@ -1,10 +1,62 @@
 //simple Matrix;
+
 #include<stdio.h>
 int main()
 {
-    int i,j,a[4][5];
+    int A[3][4],B[3][4];
+    int i,j;
+
+    printf("Enter Element of A Matrix:\n");
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            printf("A[%d][%d]=",i,j);
+            scanf("%d",&A[i][j]);
+        }
+        printf("\n");
+    }
 
 
+
+    printf("A=");
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            printf("\t %d",A[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n\n");
+
+    printf("Enter Element of B Matrix:\n");
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            printf("A[%d][%d]=",i,j);
+            scanf("%d",&B[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+
+    printf("B=");
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            printf("\t%d",B[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
 
 }
 
