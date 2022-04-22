abdc7f7034515ca9001a6712fb80b57a7d8e624d
Md. Emran Hossen <92653363+emranhossen26@users.noreply.github.com>
2022-04-23 00:31:46 +0600

    Add files via upload

---
+++ Array/Array-01.c
@@ -0,0 +1,24 @@
+//Array defination
+//Array declare
+//Array initilize
+
+//Array :Array is a Collection of variable that can store multiple values
+
+#include<stdio.h>
+int main()
+{
+    //declare
+    int num[4];
+
+    //initilize
+
+    num[0]=1;
+    num[1]=4;
+    num[2]=5;
+    num[3]=6;
+
+    printf("%d\n",num[3]);
+    printf("%d\n",num[0]);
+    return 0;
+
+}
